fd341ad8d499b95d04dd64bdd702fb4aad813463
mr-Claster <67782747+mr-Claster@users.noreply.github.com>
2021-11-28 21:29:46 +0200

    first bot done

---
+++ KpiLab_Labirint/bots/BotBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using KpiLab_Labirint.bots;
+using Stack = KpiLab_Labirint.bots.Stack;
+
+namespace KpiLab_Labirint
+{
+    abstract class BotBase
+    {
+        protected MazeTree Memory = new MazeTree();
+        protected Stack GoBack = new Stack();
+        public abstract void MakeDecision(List<Tuple<int,int>>[] derection, int SizeArray);
+    }
+}
---
+++ KpiLab_Labirint/bots/stack/Stack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KpiLab_Labirint.bots
+{
+    public class Stack
+    {
+        private List<Tuple<int, int>> Items = new List<Tuple<int, int>>();//first - length, second - derection 
+        public int Count => Items.Count;
+
+        public void Push(int length, int derection)
+        {
+            Tuple<int, int> tmp = new Tuple<int, int>(length, derection);
+            Items.Add(tmp);
+        }
+
+        public Tuple<int, int> Pop()
+        {
+            if (Count > 0)
+            {
+                return Items.LastOrDefault();
+            }
+            else
+            {
+                Tuple<int, int> tmp = new Tuple<int, int>(0, 0);
+                return tmp;
+            }
+        }
+    }
+}
